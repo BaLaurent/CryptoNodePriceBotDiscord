b90cf67bea8c3729c418f7ce69b9e568a60db05b
Octopusman388 <d3adlol0@gmail.com>
2022-04-27 16:22:24 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import discord
+import requests
+import asyncio
+import json
+
+
+client = discord.Client()
+
+showFiat = False
+
+async def get_price():
+    global TokenAddress
+    global network
+    global showFiat
+
+    url = "https://api.dexscreener.io/latest/dex/pairs/avalanche/0x98d0bdf8745e672a6e2120c492b084230e958304"
+    r = requests.get(url)
+    json_data = json.loads(r.text)
+    print("json_data :")
+    print(json_data)
+    infos = json_data["pair"]
+    print("infos :")
+    print(infos)
+    if showFiat:
+        return infos["priceUsd"]
+    else :
+        return infos["priceNative"]
+
+async def refreshStatus(): 
+    while True:
+        prix = await get_price()
+        if showFiat:
+            symbol = "$"
+        else:
+            symbol = "AVAX"
+        await client.change_presence(activity=discord.Game(name=str(prix) + " " + symbol))
+        await asyncio.sleep(5)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    client.loop.create_task(refreshStatus())
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$showFiat'):
+        global showFiat
+        showFiat = ~showFiat
+        if showFiat :
+            await message.channel.send("Showing price in Fiat")
+        else :
+            await message.channel.send("Showing price in AVAX")
+
+client.run('<YOUR DISCORD TOKEN>')
